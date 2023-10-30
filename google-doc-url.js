11629ddc10f154232e53e9f69bb7388b85b3134d
AlvaroRuC <149305775+AlvaroRuC@users.noreply.github.com>
2023-10-30 16:18:23 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1uGj1HDyyp8-DtPkJb76TjMBV6sDx91-feI7em4-uOOM/edit#gid=1033046795';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+var googleApiKey = 'AIzaSyD3aYpDY439TosDbqwd5EvFR_zwAdV5vPg';
